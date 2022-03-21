5e38f277b1a488d1f1113efc0a91fca80f2a0147
SophearithSaing <SophearithSaing@users.noreply.github.com>
2022-03-21 16:07:58 +0700

    Add spinner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Search from './components/Search';
+import Spinner from './components/Spinner';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   function search(query) {
+    setIsLoading(true);
     fetch(
       `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&query=${query}`,
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setIsLoading(false);
+      });
   }
 
   return (
     <div className="app">
       <h1>Image Search</h1>
       <Search search={search} />
+      {isLoading && <Spinner />}
     </div>
   );
 }
