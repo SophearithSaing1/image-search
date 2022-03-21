2e8175cdbff332c4bbfb59fa386e6d826e278ca6
SophearithSaing <SophearithSaing@users.noreply.github.com>
2022-03-21 15:39:16 +0700

    Add search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Search from './components/Search';
 
 function App() {
+  function search(query) {
+    fetch(
+      `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&query=${query}`,
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Image Search</h1>
+      <Search search={search} />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './Search.css';
+
+function Search({ search }) {
+  const [query, setQuery] = useState('');
+
+  function inputChangeHandler(event) {
+    setQuery(event.target.value);
+  }
+
+  function searchHandler() {
+    search(query);
+    setQuery('');
+  }
+
+  function keyUpHandler(event) {
+    if (event.key === 'Enter') {
+      searchHandler();
+    }
+  }
+
+  return (
+    <div className="search">
+      <input
+        className="search--input"
+        type="text"
+        placeholder="Enter keywords"
+        value={query}
+        onChange={inputChangeHandler}
+        onKeyUp={keyUpHandler}
+      />
+      <button className="search--button" onClick={searchHandler}>
+        Search
+      </button>
+    </div>
+  );
+}
+
+export default Search;
